ee734a59f6ab18265b269f5dd8d9d5d6071cc1bd
shangxin <shangxin@outlook.com>
2025-12-26 00:18:02 +0800

    build: allow test file

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,9 @@ export default {
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.js', '**/?(*.)+(spec|test).js'],
   transform: {},
-  // eÍ\
   forceExit: true,
   detectOpenHandles: true,
-  //  …ööôåeÍ\
   testTimeout: 10000,
-  // D
   clearMocks: true,
   restoreMocks: true,
 };
